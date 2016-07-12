b51c4df0058651f22b71bc7a493145b6eb584b0b
prateeks <prateek.s@hirepro.in>
2016-07-12 18:23:59 +0530

    promisify implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-var restify = require('restify');
+// var restify = require('restify');
 
-var server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+// var server = restify.createServer({
+//   name: 'myapp',
+//   version: '1.0.0'
+// });
+// server.use(restify.acceptParser(server.acceptable));
+// server.use(restify.queryParser());
+// server.use(restify.bodyParser());
 
-// server.get('/echo/:name', function (req, res, next) {
-//   res.send(req.params);
-//   return next();
+// // server.get('/echo/:name', function (req, res, next) {
+// //   res.send(req.params);
+// //   return next();
+// // });
+
+// server.listen(8080, function () {
+//   console.log('%s listening at %s', server.name, server.url);
 // });
 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+// var server = require('./server')().then(() => {
+//     console.log('Server stared !');
+// });
+var server = require('./server');
+var promise = server();
+console.log('promise = ', promise);
+promise.then((srv) => {
+	console.log('My Server started!!!');
 });
+//server;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
 'use strict'
 
 var restify = require("restify");
+var Promise = require('bluebird');
 
-module.export = function() {
+module.exports = function() {
     var server = restify.createServer({
         name: 'myapp',
         version: '1.0.0'
     });
-
     server.use(restify.acceptParser(server.acceptable));
     server.use(restify.queryParser());
     server.use(restify.bodyParser());
 
-    return server;
+    // server.get('/echo/:name', function (req, res, next) {
+    //   res.send(req.params);
+    //   return next();
+    // });
+
+    return new Promise((resolve) => {
+        server.listen(8080, function() {
+            console.log('%s listening at %s', server.name, server.url);
+            return resolve(server);
+        });
+    });
 }
